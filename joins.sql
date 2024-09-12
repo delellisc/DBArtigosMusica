ad83a21043851624d811cb5e9ea4d017c27b7255
de Lellis <delellis021@gmail.com>
2024-09-12 06:57:05 -0300

    Edição de formatação

--- joins.sql
+++ joins.sql
@@ -3,11 +3,13 @@
 SELECT p.nome AS produto, c.nome AS categoria
 FROM produto p
 INNER JOIN categoria c ON p.categoria_id = c.categoria_id;
+
 -- seleciona cliente e funcionario envolvidos num pedido
 SELECT pe.pedido_id, cl.nome AS cliente, f.nome AS funcionario
 FROM pedido pe
 INNER JOIN cliente cl ON pe.cliente_id = cl.cliente_id
 INNER JOIN funcionario f ON pe.funcionario_id = f.funcionario_id;
+
 -- seleciona valor e data de pagamento de um pedido
 SELECT pe.pedido_id, pa.valor AS valor_pagamento, pa.data_pagamento
 FROM pedido pe
